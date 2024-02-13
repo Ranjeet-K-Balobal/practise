3c5c9f5ddae533dbe72349142c4e0e074b9612f7
Ranjeet-K-Balobal <ranjeetbalo600@gmail.com>
2024-02-13 19:12:19 +0530

    codes updated

---
+++ test-2/App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Test-2 is Runing!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
